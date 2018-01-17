e5b5377c6c510a98650df9b1b42a028cb5df6bd8
gFelicio <gustavo.a.felicio@gmail.com>
2018-01-17 14:22:29 -0200

    server.js update

--- server.js
+++ server.js
@@ -1,15 +1,2 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res){
-    res.render('index');
-});
-
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000!');
-});
-
+// Copy the content from server.example.js here
+// replace the apiKey with the key provided for you int he openweather map website
